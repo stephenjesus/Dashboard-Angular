93f6e57ee8896936b91667038d67fa223ba21d6c
malavancs <mlvnhari@gmail.com>
2019-08-23 23:23:38 +0530

    Initial Comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,84 +1,84 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgReduxModule} from '@angular-redux/store';
-import {NgRedux, DevToolsExtension} from '@angular-redux/store';
-import {rootReducer, ArchitectUIState} from './ThemeOptions/store';
-import {ConfigActions} from './ThemeOptions/store/config.actions';
-import {AppRoutingModule} from './app-routing.module';
-import {LoadingBarRouterModule} from '@ngx-loading-bar/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgReduxModule } from '@angular-redux/store';
+import { NgRedux, DevToolsExtension } from '@angular-redux/store';
+import { rootReducer, ArchitectUIState } from './ThemeOptions/store';
+import { ConfigActions } from './ThemeOptions/store/config.actions';
+import { AppRoutingModule } from './app-routing.module';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
 
-import {CommonModule} from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
-import {AppComponent} from './app.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
 
 // BOOTSTRAP COMPONENTS
 
-import {AngularFontAwesomeModule} from 'angular-font-awesome';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
-import {PERFECT_SCROLLBAR_CONFIG} from 'ngx-perfect-scrollbar';
-import {PerfectScrollbarConfigInterface} from 'ngx-perfect-scrollbar';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 
 // LAYOUT
 
-import {BaseLayoutComponent} from './Layout/base-layout/base-layout.component';
-import {PagesLayoutComponent} from './Layout/pages-layout/pages-layout.component';
-import {PageTitleComponent} from './Layout/Components/page-title/page-title.component';
+import { BaseLayoutComponent } from './Layout/base-layout/base-layout.component';
+import { PagesLayoutComponent } from './Layout/pages-layout/pages-layout.component';
+import { PageTitleComponent } from './Layout/Components/page-title/page-title.component';
 
 // HEADER
 
-import {HeaderComponent} from './Layout/Components/header/header.component';
-import {SearchBoxComponent} from './Layout/Components/header/elements/search-box/search-box.component';
-import {UserBoxComponent} from './Layout/Components/header/elements/user-box/user-box.component';
+import { HeaderComponent } from './Layout/Components/header/header.component';
+import { SearchBoxComponent } from './Layout/Components/header/elements/search-box/search-box.component';
+import { UserBoxComponent } from './Layout/Components/header/elements/user-box/user-box.component';
 
 // SIDEBAR
 
-import {SidebarComponent} from './Layout/Components/sidebar/sidebar.component';
-import {LogoComponent} from './Layout/Components/sidebar/elements/logo/logo.component';
+import { SidebarComponent } from './Layout/Components/sidebar/sidebar.component';
+import { LogoComponent } from './Layout/Components/sidebar/elements/logo/logo.component';
 
 // FOOTER
 
-import {FooterComponent} from './Layout/Components/footer/footer.component';
+import { FooterComponent } from './Layout/Components/footer/footer.component';
 
 // DEMO PAGES
 
 // Dashboards
 
-import {AnalyticsComponent} from './DemoPages/Dashboards/analytics/analytics.component';
+import { AnalyticsComponent } from './DemoPages/Dashboards/analytics/analytics.component';
 
 // Pages
 
-import {ForgotPasswordBoxedComponent} from './DemoPages/UserPages/forgot-password-boxed/forgot-password-boxed.component';
-import {LoginBoxedComponent} from './DemoPages/UserPages/login-boxed/login-boxed.component';
-import {RegisterBoxedComponent} from './DemoPages/UserPages/register-boxed/register-boxed.component';
+import { ForgotPasswordBoxedComponent } from './DemoPages/UserPages/forgot-password-boxed/forgot-password-boxed.component';
+import { LoginBoxedComponent } from './DemoPages/UserPages/login-boxed/login-boxed.component';
+import { RegisterBoxedComponent } from './DemoPages/UserPages/register-boxed/register-boxed.component';
 
 // Elements
 
-import {StandardComponent} from './DemoPages/Elements/Buttons/standard/standard.component';
-import {DropdownsComponent} from './DemoPages/Elements/dropdowns/dropdowns.component';
-import {CardsComponent} from './DemoPages/Elements/cards/cards.component';
-import {ListGroupsComponent} from './DemoPages/Elements/list-groups/list-groups.component';
-import {TimelineComponent} from './DemoPages/Elements/timeline/timeline.component';
-import {IconsComponent} from './DemoPages/Elements/icons/icons.component';
+import { StandardComponent } from './DemoPages/Elements/Buttons/standard/standard.component';
+import { DropdownsComponent } from './DemoPages/Elements/dropdowns/dropdowns.component';
+import { CardsComponent } from './DemoPages/Elements/cards/cards.component';
+import { ListGroupsComponent } from './DemoPages/Elements/list-groups/list-groups.component';
+import { TimelineComponent } from './DemoPages/Elements/timeline/timeline.component';
+import { IconsComponent } from './DemoPages/Elements/icons/icons.component';
 
 // Components
 
 
 // Tables
 
-import {RegularComponent} from './DemoPages/Tables/regular/regular.component';
-import {TablesMainComponent} from './DemoPages/Tables/tables-main/tables-main.component';
+import { RegularComponent } from './DemoPages/Tables/regular/regular.component';
+import { TablesMainComponent } from './DemoPages/Tables/tables-main/tables-main.component';
 
 // Widgets
 
-import {ChartBoxes3Component} from './DemoPages/Widgets/chart-boxes3/chart-boxes3.component';
+import { ChartBoxes3Component } from './DemoPages/Widgets/chart-boxes3/chart-boxes3.component';
 
 // Forms Elements
 
-import {ControlsComponent} from './DemoPages/Forms/Elements/controls/controls.component';
-import {LayoutComponent} from './DemoPages/Forms/Elements/layout/layout.component';
+import { ControlsComponent } from './DemoPages/Forms/Elements/controls/controls.component';
+import { LayoutComponent } from './DemoPages/Forms/Elements/layout/layout.component';
 
 // Charts
 
@@ -88,6 +88,12 @@ import { ProfileComponent } from './profile/profile.component';
 import { SignupComponent } from './signup/signup.component';
 import { UserdashboardComponent } from './userdashboard/userdashboard.component';
 import { GrowlShowComponent } from './growlcompoent/growl';
+
+
+import { AngularFireModule } from '@angular/fire';
+
+import { AngularFireStorageModule, StorageBucket } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true
@@ -177,27 +183,30 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireStorageModule
     // Charts
 
   ],
   providers: [
     {
       provide:
-      PERFECT_SCROLLBAR_CONFIG,
+        PERFECT_SCROLLBAR_CONFIG,
       // DROPZONE_CONFIG,
       useValue:
-      DEFAULT_PERFECT_SCROLLBAR_CONFIG,
+        DEFAULT_PERFECT_SCROLLBAR_CONFIG,
+
       // DEFAULT_DROPZONE_CONFIG,
     },
     ConfigActions,
+    { provide: StorageBucket, useValue: 'online-management-a7e84' }
   ],
   bootstrap: [AppComponent]
 })
 
 export class AppModule {
   constructor(private ngRedux: NgRedux<ArchitectUIState>,
-              private devTool: DevToolsExtension) {
+    private devTool: DevToolsExtension) {
 
     this.ngRedux.configureStore(
       rootReducer,
